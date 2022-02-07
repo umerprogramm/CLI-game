20e50ed581e5b58f1228af8ef0ac284b1bbdc6b8
Umer <umerprogrammer@gmail.com>
2022-02-07 09:47:25 -0800

    first commit

--- cli_game.py
+++ cli_game.py
@@ -1,5 +1,32 @@
+
 import json
+
+print('\t\t\t\tWellcome to my game😊\t\t\t\t\t')
 
 with open('data.json') as json_file:
     data = json.load(json_file)
-    print(data)
+    print(data['ques_1']['explanation'])
+    print(data["ques_1"]["option_1"])
+    print(data["ques_1"]["option_2"])
+    print(data["ques_1"]["option_3"])
+    inp = input('eneter the correct option')
+    if inp =="2" or inp== "3":
+        print('you lose the game 😪')
+    
+    elif inp=="1":
+        print('Nice try 👏')
+        print(data['ques_2']['explanation'])
+        print(data["ques_2"]["option_1"])
+        
+        inp1 = input('')
+        if inp1 =="t.count(2)":
+             print('Nice try 👏')
+             print(data['ques_3']['explanation'])
+             print(data["ques_3"]["option_1"])
+             print(data["ques_3"]["option_2"])
+             inp2 = input('chioce the correct option')
+             if inp2=="2":
+                 print('You are the winner of this Gmae please take your crown 👑')
+    
+        else:
+             print('you lose the game 😪')
